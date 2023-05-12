d03bd4b29a1ad32aa0a1b1be000fe507af6cc2ce
ATFfang <643737159@qq.com>
2023-05-12 21:57:25 +0800

    1st

---
+++ flowJS.js
@@ -0,0 +1,8 @@
+		mapboxgl.accessToken = 'pk.eyJ1IjoiYXRmaWVsZDIwMjIiLCJhIjoiY2xlZjFodW1lMDR3dTNvbXVvajMwNGxzZSJ9.1FnjGYOuY7l-Us1SFatgKg';
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/navigation-night-v1',
+            center: [112,36],
+            zoom: 3
+        });
+
