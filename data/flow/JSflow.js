9ea0b963648061c81b7cb7ceda73e62cd6fb6f88
Fangtianyao <106510070+ATFfang@users.noreply.github.com>
2023-06-12 10:51:50 +0800

    Add files via upload

--- data/flow/JSflow.js
+++ data/flow/JSflow.js
@@ -1,5 +1,31 @@
-function GetFlowData()
+class cityFlowData{
+    constructor(cityCode,odPoint,flowValue,flowType,flowTime){
+        this.cityCode=cityCode;
+        this.odPoint=odPoint;
+        this.flowValue=flowValue;
+        this.flowType=flowType;
+        this.flowTime=flowTime;
+    }
+}
+
+function getFlowData()
 {
+    fetch('flow.json')
+  .then(response => response.json())
+  .then(jsonData => {
+    for (let i = 0; i < jsonData.length; i++) {
+      const item = jsonData[i];
+      console.log(item);
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
+function GetFlowData2()
+{
+    const cityCode=110000;
     const lineList = [
         [[116.38,39.87], [121.46,31.25]], // 第一条线条的起始点和结束点坐标
         [[116.38,39.87], [104.07,30.57]],
@@ -13,11 +39,15 @@ function GetFlowData()
         [[116.38,39.87], [114.30,30.59]],
         [[116.38,39.87], [91.12,29.65]],
         [[116.38,39.87], [113.26,23.13]],
-        [[116.38,39.87], [109.95,40.62]],
+        [[116.38,39.87], [109.95,40.62]]
     ];
-    
-    const flowValue=[7,4,3,2,3,2,4,2,3,5,2,4,2];
-    
+    const flowValue=[7,4.5,3.2,2,3,2,4,2,3,5.13254,2,4,2];
+    const flowType="warm";
+    const flowTime="2020-5-1"
+
+    const cd1=new cityFlowData(cityCode,lineList,flowValue,flowType,flowTime)
+
+    const cityCode2=310000;
     const lineList2 = [
         [[121.46,31.25], [116.38,39.87]], // 第一条线条的起始点和结束点坐标
         [[121.46,31.25], [104.07,30.57]],
@@ -35,7 +65,12 @@ function GetFlowData()
     ];
     
     const flowValue2=[7,2,3,2,3,2,4,2,3,5,2,4,2];
-    
+    const flowType2="cold";
+    const flowTime2="2020-5-1"
+
+    const cd2=new cityFlowData(cityCode2,lineList2,flowValue2,flowType2,flowTime2)
+
+    const cityCode3=532900;
     const lineList3 = [
         [[100.27,25.61], [116.38,39.87]], // 第一条线条的起始点和结束点坐标
         [[100.27,25.61], [104.07,30.57]],
@@ -53,13 +88,21 @@ function GetFlowData()
     ];
     
     const flowValue3=[4,2,3,2,3,2,4,2,6,5,2,4,2];
-    
-    
-    var Datatuple=[lineList,flowValue];
-    var Datatuple2=[lineList2,flowValue2];
-    var Datatuple3=[lineList3,flowValue3];
-    
-    var DatatupleList=[Datatuple,Datatuple2,Datatuple3];
+    const flowType3="cold";
+    const flowTime3="2020-5-1"
 
-    return DatatupleList;
+    const cd3=new cityFlowData(cityCode3,lineList3,flowValue3,flowType3,flowTime3)
+
+    citylist=[cd1,cd2,cd3]
+
+
+    //用map类型存放cityFlowData的数据，一个cityFlowData代表一个城市向外的流动
+
+    const dictionary = new Map();
+    for (const city of citylist) {
+        dictionary.set(city.cityCode,city);
+    } 
+
+    return dictionary
 }
+
